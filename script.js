c71f4cf2f465ab3224a0dac85c39c0e6f324e5fd
Wojtek Bardyga <wojtek.bardyga@gmail.com>
2023-03-26 20:11:08 +0200

    Modify Start button

---
+++ script.js
@@ -0,0 +1,38 @@
+const startBtn = document.querySelector('.start')
+const pauseBtn = document.querySelector('.pause')
+const stopBtn = document.querySelector('.stop')
+const removeBtn = document.querySelector('.remove')
+const archiveBtn = document.querySelector('.archive')
+const stopwatch = document.querySelector('.stopwatch')
+const time = document.querySelector('.time')
+const timeList = document.querySelector('.time-list')
+
+const infoBtn = document.querySelector('.info')
+const modalShawdow = document.querySelector('.modal-shawdow')
+const closeModalBtn = document.querySelector('.close')
+
+let countTime;
+let minutes = 0;
+let seconds = 0;
+
+const handleStart = () => { 
+
+    countTime = setInterval(() => {
+        
+        if(seconds < 9) {
+            seconds++
+            stopwatch.textContent = `${minutes}:0${seconds}`
+        } else if (seconds >= 9 && seconds < 59) {
+            seconds++
+            stopwatch.textContent = `${minutes}:${seconds}`
+        } else {
+            minutes ++;
+            seconds = 0;
+            stopwatch.textContent = `${minutes}:00`
+        }
+
+    }, 1000)
+ }
+
+
+ startBtn.addEventListener('click', handleStart)
